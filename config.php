ff2d4ac9f9476ee02912854b690e0fec0a8985aa
HelibenModi <helimodi710@gmail.com>
2025-05-23 21:17:36 +0000

    added initial database configuration

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_Host', 'si1dy.h.filess.io');
+define('DB_User', 'COSC4806_cryfogmove');
+define('DB_Pass', '$_ENV['DB_Pass']);
+define('DB_DATABASE', 'COSC4806_cryfogmove');
+define('DB_Port', '61000');
+
+  ?>
